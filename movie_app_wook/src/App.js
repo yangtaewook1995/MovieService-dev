50ec7d2c587bcf4239066ea2b1e10f88b45b3980
Wook <wooke1995@gmail.com>
2021-08-16 16:08:23 +0900

    state

--- movie_app_wook/src/App.js
+++ movie_app_wook/src/App.js
@@ -1,64 +1,39 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
-const foodLike = [
-    {
-        id : 1,
-        name : "kimchi", 
-        image : "http://img.danawa.com/prod_img/500000/830/590/img/5590830_1.jpg?shrink=360:360&_v=20210111143615",
-        rating : 4.0,
-    },
-    {
-        id : 2,
-        name : "ramen", 
-        image : "",
-        rating : 5.0,
 
-    },    
-    {
-        id : 3,
-        name : "meat", 
-        image : "",
-        rating : 3.5,
+class App extends React.Component {
+    state = {
+        count : 0
+    };
 
-    },    
-    {
-        id : 4,
-        name : "milk", 
-        image : "",
-        rating : 4.0,
-
+    add = () => {
+        this.setState(current => ({count : current.count + 1}));
     }
-]
 
-function Food(props) {
-    return (
-        <div>
-            <h2>I love {props.name}</h2>
-            <h4>{props.rating}/5</h4>
-            <img src={props.picture} />
-        </div>
-    )
-}
+    sub = () => {
+        this.setState(current => ({count : current.count - 1}));
+    }
 
-function renderFood(dish) {
-    return(
-        <Food name={dish.name} picture={dish.image} id={dish.id} rating={dish.rating}/>
-    )
-}
+    componentDidMount() {
+        console.log("render finished");
+    }
 
-Food.propTypes = {
-    name : PropTypes.string.isRequired,
-    picture : PropTypes.string.isRequired,
-    rating : PropTypes.number
-}
+    componentDidUpdate() {
+        console.log("component update");
+    }
 
-function App() {
-    return (
-        <div>
-            {foodLike.map(renderFood)}
-        </div>
-    );
+    render() {
+        return (
+            <div>
+                <h1>The number is {this.state.count}</h1>
+                <button onClick={this.add}>Add</button>
+                <button onClick={this.sub}>Sub</button>
+            </div>
+        )
+    }
+
+
 }
 
 export default App;
