97cc66297513ca7f42383723c7b48e9dec60c998
Wook <wooke1995@gmail.com>
2021-08-16 15:37:41 +0900

    rendering

--- movie_app_wook/src/App.js
+++ movie_app_wook/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from "prop-types";
+
+const foodLike = [
+    {
+        id : 1,
+        name : "kimchi", 
+        image : "http://img.danawa.com/prod_img/500000/830/590/img/5590830_1.jpg?shrink=360:360&_v=20210111143615",
+        rating : 4.0,
+    },
+    {
+        id : 2,
+        name : "ramen", 
+        image : "",
+        rating : 5.0,
+
+    },    
+    {
+        id : 3,
+        name : "meat", 
+        image : "",
+        rating : 3.5,
+
+    },    
+    {
+        id : 4,
+        name : "milk", 
+        image : "",
+        rating : 4.0,
+
+    }
+]
+
+function Food(props) {
+    return (
+        <div>
+            <h2>I love {props.name}</h2>
+            <h4>{props.rating}/5</h4>
+            <img src={props.picture} />
+        </div>
+    )
+}
+
+function renderFood(dish) {
+    return(
+        <Food name={dish.name} picture={dish.image} id={dish.id} rating={dish.rating}/>
+    )
+}
+
+Food.propTypes = {
+    name : PropTypes.string.isRequired,
+    picture : PropTypes.string.isRequired,
+    rating : PropTypes.number
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            {foodLike.map(renderFood)}
+        </div>
+    );
 }
 
 export default App;
